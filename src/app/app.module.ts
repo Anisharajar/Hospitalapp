fa3977907205df228e4a69749c1fb71aa3ba7806
Anisharajar <anisharajar.2002@gmail.com>
2022-07-08 00:43:15 +0530

    Search doctor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewdrComponent } from './viewdr/viewdr.component';
+import { SearchdrComponent } from './searchdr/searchdr.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewdrComponent
+    ViewdrComponent,
+    SearchdrComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/searchdr/searchdr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-searchdr',
+  templateUrl: './searchdr.component.html',
+  styleUrls: ['./searchdr.component.css']
+})
+export class SearchdrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
